4b520868883a5f92588be19d2bcfcf347dc22dca
Matthew Ito <mito81@gmail.com>
2022-10-15 08:47:10 -0700

    First commit
    The first commit.

---
+++ src/components/EmojiPicker.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const state = reactive({ count: 0 });
+</script>
+
+<template>
+    <p>{{state.count}}</p>
+    <button @click="() => {state.count++}">Click me</button>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
